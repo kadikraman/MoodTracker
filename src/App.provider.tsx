2195d951da4f2933f9d0eb053593a74998531af7
Kadi Kraman <hellokadi@gmail.com>
2021-09-26 21:15:07 +0100

    Persist data across app launches

--- src/App.provider.tsx
+++ src/App.provider.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
 import { MoodOptionType, MoodOptionWithTimestamp } from './types';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const storageKey = 'my-app-data';
+
+type AppData = {
+  moods: MoodOptionWithTimestamp[];
+};
+
+const getAppData = async (): Promise<AppData | null> => {
+  try {
+    const data = await AsyncStorage.getItem(storageKey);
+
+    if (data) {
+      return JSON.parse(data);
+    }
+    return null;
+  } catch {
+    return null;
+  }
+};
+
+const setAppData = async (newData: AppData) => {
+  try {
+    await AsyncStorage.setItem(storageKey, JSON.stringify(newData));
+  } catch {}
+};
 
 type AppContextType = {
   moodList: MoodOptionWithTimestamp[];
@@ -17,7 +43,22 @@ export const AppProvider: React.FC = ({ children }) => {
   const [moodList, setMoodList] = React.useState<MoodOptionWithTimestamp[]>([]);
 
   const handleSelectMood = React.useCallback((mood: MoodOptionType) => {
-    setMoodList(current => [...current, { mood, timestamp: Date.now() }]);
+    setMoodList(current => {
+      const newValue = [...current, { mood, timestamp: Date.now() }];
+      setAppData({ moods: newValue });
+      return newValue;
+    });
+  }, []);
+
+  React.useEffect(() => {
+    const getDataFromStorage = async () => {
+      const data = await getAppData();
+
+      if (data) {
+        setMoodList(data.moods);
+      }
+    };
+    getDataFromStorage();
   }, []);
 
   return (
