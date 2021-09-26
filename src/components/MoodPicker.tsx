da149e5545e90a99ea33439feddcdf3d56b85224
Kadi Kraman <hellokadi@gmail.com>
2021-09-26 21:57:51 +0100

    Show a background image with ImageBackground

--- src/components/MoodPicker.tsx
+++ src/components/MoodPicker.tsx
@@ -104,6 +104,7 @@ const styles = StyleSheet.create({
     padding: 20,
     justifyContent: 'space-between',
     height: 230,
+    backgroundColor: 'rgba(0,0,0,0.2)',
   },
   heading: {
     fontSize: 20,
@@ -111,6 +112,7 @@ const styles = StyleSheet.create({
     letterSpacing: 1,
     textAlign: 'center',
     marginBottom: 20,
+    color: theme.colorWhite,
   },
   button: {
     backgroundColor: theme.colorPurple,
--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import { useAppContext } from '../App.provider';
 import { MoodPicker } from '../components/MoodPicker';
+
+const imageUrl =
+  'https://images.unsplash.com/photo-1474540412665-1cdae210ae6b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1766&q=80';
 
 export const Home: React.FC = () => {
   const appContext = useAppContext();
 
   return (
-    <View style={styles.container}>
+    <ImageBackground source={{ uri: imageUrl }} style={styles.container}>
       <MoodPicker onSelect={appContext.handleSelectMood} />
-    </View>
+    </ImageBackground>
   );
 };
 
