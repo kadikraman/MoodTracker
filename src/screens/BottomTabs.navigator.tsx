51529ed7153b3b4796229e8c2f6aa8db6a96911d
Kadi Kraman <hellokadi@gmail.com>
2021-09-26 22:13:44 +0100

    Use a vector icon for the home tab

--- src/screens/BottomTabs.navigator.tsx
+++ src/screens/BottomTabs.navigator.tsx
@@ -1,17 +1,52 @@
 import React from 'react';
+import { Text } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Home } from './Home.screen';
 import { History } from './History.screen';
 import { Analytics } from './Analytics.screen';
+import { HomeIcon } from '../components/Icons';
+import { theme } from '../theme';
 
 const BottomTabs = createBottomTabNavigator();
 
 export const BottomTabsNavigator: React.FC = () => {
   return (
-    <BottomTabs.Navigator>
-      <BottomTabs.Screen name="Home" component={Home} />
-      <BottomTabs.Screen name="History" component={History} />
-      <BottomTabs.Screen name="Analytics" component={Analytics} />
+    <BottomTabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: theme.colorBlue,
+        tabBarInactiveTintColor: theme.colorGrey,
+        tabBarShowLabel: false,
+        tabBarIcon: ({ color, size }) => {
+          if (route.name === 'Home') {
+            return <HomeIcon color={color} size={size} />;
+          }
+
+          if (route.name === 'History') {
+            return <Text>History</Text>;
+          }
+
+          if (route.name === 'Analytics') {
+            return <Text>Analytics</Text>;
+          }
+
+          return null;
+        },
+      })}>
+      <BottomTabs.Screen
+        name="Home"
+        component={Home}
+        options={{ title: "Today's Mood" }}
+      />
+      <BottomTabs.Screen
+        name="History"
+        component={History}
+        options={{ title: 'Past Moods' }}
+      />
+      <BottomTabs.Screen
+        name="Analytics"
+        component={Analytics}
+        options={{ title: 'Fancy Charts' }}
+      />
     </BottomTabs.Navigator>
   );
 };
--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,7 @@
 export const theme = {
   colorPurple: '#454C73',
   colorLavender: '#87677B',
+  colorBlue: '#1D84B5',
+  colorGrey: '#8E9AAF',
   colorWhite: '#fff',
 };
