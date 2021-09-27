75ba6b1dff009ec0ebc6f653f6085fe5eb03c7b6
Kadi Kraman <hellokadi@gmail.com>
2021-09-27 20:17:15 +0100

    Add a pie chart

--- src/screens/Analytics.screen.tsx
+++ src/screens/Analytics.screen.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { VictoryPie } from 'victory-native';
+import groupBy from 'lodash/groupBy';
+import { useAppContext } from '../App.provider';
+import { theme } from '../theme';
 
 export const Analytics: React.FC = () => {
+  const appContext = useAppContext();
+
+  const data = Object.entries(groupBy(appContext.moodList, 'mood.emoji')).map(
+    ([key, value]) => ({
+      x: key,
+      y: value.length,
+    }),
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Analytics</Text>
+      <VictoryPie
+        labelRadius={80}
+        radius={150}
+        innerRadius={50}
+        colorScale={[
+          theme.colorPurple,
+          theme.colorLavender,
+          theme.colorBlue,
+          theme.colorGrey,
+          theme.colorWhite,
+        ]}
+        style={{ labels: { fontSize: 30 } }}
+        data={data}
+      />
     </View>
   );
 };
@@ -12,5 +38,7 @@ export const Analytics: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
